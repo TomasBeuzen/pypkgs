a2b22e45c6bbdfbbc0abcce849b672443ea9b340
Tomas Beuzen <tomas.beuzen@gmail.com>
2020-07-31 15:47:25 -0700

    add GH actions

--- pypkgs/pypkgs.py
+++ pypkgs/pypkgs.py
@@ -20,6 +20,7 @@ def catbind(a, b):
     Examples
     --------
     >>> from pypkgs import pypkgs
+    >>> import pandas as pd
     >>> a = pd.Categorical(["character", "hits", "your", "eyeballs"])
     >>> b = pd.Categorical(["but", "integer", "where it", "counts"])
     >>> pypkgs.catbind(a, b)
@@ -27,8 +28,10 @@ def catbind(a, b):
     Categories (8, object): [but, character, counts,
     eyeballs, hits, integer, where it, your]
     """
+
     if not all(isinstance(x, pd.Categorical) for x in (a, b)):
         raise TypeError("Inputs should be of type 'Pandas categorical'.")
 
-    concatenated = pd.concat([pd.Series(a.astype("str")), pd.Series(b.astype("str"))])
+    concatenated = pd.concat([pd.Series(a.astype("str")),
+                              pd.Series(b.astype("str"))])
     return pd.Categorical(concatenated)
--- tests/test_pypkgs.py
+++ tests/test_pypkgs.py
@@ -3,22 +3,39 @@ from pypkgs import pypkgs
 import pandas as pd
 from pytest import raises, fixture
 
+
 @fixture()
 def test_data():
-    return {'a': pd.Categorical(["character", "hits", "your", "eyeballs"]),
-            'b': pd.Categorical(["but", "integer", "where it", "counts"]),
-            'c': ["but", "integer", "where it", "counts"]}
+    return {
+        "a": pd.Categorical(["character", "hits", "your", "eyeballs"]),
+        "b": pd.Categorical(["but", "integer", "where it", "counts"]),
+        "c": ["but", "integer", "where it", "counts"],
+    }
+
 
 class Testpypkgs:
     def test_version(self):
-        assert __version__ == '0.1.0'
+        assert __version__ == "0.1.1"
 
     def test_catbind(self, test_data):
-        assert ((pypkgs.catbind(test_data['a'], test_data['b'])).codes == 
-                [1, 4, 7, 3, 0, 5, 6, 2]).all()
-        assert ((pypkgs.catbind(test_data['a'], test_data['b'])).categories == 
-                ["but", "character", "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
+        assert (
+            (pypkgs.catbind(test_data["a"], test_data["b"])).codes
+            == [1, 4, 7, 3, 0, 5, 6, 2]
+        ).all()
+        assert (
+            (pypkgs.catbind(test_data["a"], test_data["b"])).categories
+            == [
+                "but",
+                "character",
+                "counts",
+                "eyeballs",
+                "hits",
+                "integer",
+                "where it",
+                "your",
+            ]
+        ).all()
 
     def test_input_type(self, test_data):
         with raises(TypeError):
-            pypkgs.catbind(test_data['a'], test_data['c'])
+            pypkgs.catbind(test_data["a"], test_data["c"])
